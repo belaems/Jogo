9d5a3879df106f44099accd56fbc0ed477731a97
belaems <isabelaesterms@gmail.com>
2025-07-23 09:28:19 -0300

    score

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
+let score = 0;
+let gameOver = false;
+let scored = false; // nova flag para controlar pontuação por obstáculo
+
+const scoreElement = document.getElementById("score");
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
+
 const jump = () => {
   mario.classList.add("jump");
   setTimeout(() => {
@@ -13,16 +19,33 @@ const loop = setInterval(() => {
     .getComputedStyle(mario)
     .bottom.replace("px", "");
 
+  // Verifica colisão
   if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-    pipe.style.left = `${pipePosition}px`;
+    gameOver = true;
 
+    pipe.style.left = `${pipePosition}px`;
     pipe.style.animation = "none";
     mario.style.bottom = `${marioPosition}px`;
 
     mario.src = "./images/game-over.png";
     mario.style.width = "75px";
     mario.style.marginLeft = "50px";
+
     clearInterval(loop);
   }
+
+  // Quando o cano passa completamente e ainda não pontuou
+  if (pipePosition < 0 && !scored && !gameOver) {
+    score++;
+    scoreElement.textContent = score;
+    scored = true;
+  }
+
+  // Quando o cano estiver voltando, resetamos o controle de pontuação
+  if (pipePosition > 120) {
+    scored = false;
+  }
+
 }, 10);
+
 document.addEventListener("keydown", jump);
